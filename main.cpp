5519f3904a954d185f8b766059897e8fa13912fd
mdmassaro <143455023+mdmassaro@users.noreply.github.com>
2026-09-23 10:36:15 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
+#include "Lock.h"
+#include "CodeLock.h"
+#include "KeyLock.h"
 using namespace std;
 
-
-
-
-
 int main() {
-
+  CodeLock newLock(1234);
+  KeyLock anotherLock(5678);
 
   return 0;
 }
